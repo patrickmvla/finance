b0de54bbc016de2607d38b1bddccbadf21e1fabd
patrick mvula <mvlapatrick@gmail.com>
2024-06-17 17:12:40 +0200

    providers

---
+++ providers/sheet-provider.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { useMountedState } from "react-use";
+
+import { NewAccountSheet } from "@/features/accounts/components/new-account-sheet";
+import { EditAccountSheet } from "@/features/accounts/components/edit-account-sheet";
+
+import { NewCategorySheet } from "@/features/categories/components/new-category-sheet";
+import { EditCategorySheet } from "@/features/categories/components/edit-category-sheet";
+
+import { NewTransactionSheet } from '@/features/transactions/components/new-transaction-sheet';
+import { EditTransactionSheet } from '@/features/transactions/components/edit-transaction-sheet';
+
+export const SheetProvider = () => {
+  const isMounted = useMountedState();
+
+  if (!isMounted) return null;
+  return (
+    <>
+      <NewAccountSheet />
+      <EditAccountSheet />
+      <NewCategorySheet />
+      <EditCategorySheet />
+      <NewTransactionSheet />
+      <EditTransactionSheet />
+    </>
+  );
+};
