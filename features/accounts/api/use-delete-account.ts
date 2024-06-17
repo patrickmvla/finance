5ab9b2012f3f942dcd10da20a307ee7eb30bb420
patrick mvula <mvlapatrick@gmail.com>
2024-06-17 17:23:43 +0200

    accounts api

---
+++ features/accounts/api/use-delete-account.ts
@@ -0,0 +1,34 @@
+import { toast } from "sonner";
+import { InferResponseType } from "hono";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+
+import { client } from "@/lib/hono";
+
+type ResponseType = InferResponseType<
+  (typeof client.api.accounts)[":id"]["$delete"]
+>;
+
+export const useDeleteAccount = (id?: string) => {
+  const queryClient = useQueryClient();
+
+  const mutation = useMutation<ResponseType, Error>({
+    mutationFn: async () => {
+      const response = await client.api.accounts[":id"]["$delete"]({
+        param: { id: id },
+      });
+      return await response.json();
+    },
+    onSuccess: () => {
+      toast.success("Account deleted");
+      queryClient.invalidateQueries({ queryKey: ["account", { id }] });
+      queryClient.invalidateQueries({ queryKey: ["accounts"] });
+      queryClient.invalidateQueries({ queryKey: ["transactions"] });
+      // TODO: Invalidate summary
+    },
+    onError: () => {
+      toast.error("Failed to delete account");
+    },
+  });
+
+  return mutation;
+};
