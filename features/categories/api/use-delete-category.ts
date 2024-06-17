a5dc565a4121706aef235339570fbb52587d186f
patrick mvula <mvlapatrick@gmail.com>
2024-06-17 17:48:14 +0200

    categories api hooks and components

---
+++ features/categories/api/use-delete-category.ts
@@ -0,0 +1,35 @@
+import { toast } from "sonner";
+import { InferResponseType } from 'hono';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+
+import { client } from '@/lib/hono';
+
+type ResponseType = InferResponseType<typeof client.api.categories[":id"]["$delete"]>
+
+export const useDeleteCategory = (id?: string) => {
+  const queryClient = useQueryClient();
+
+  const mutation = useMutation<
+    ResponseType,
+    Error
+  >({
+    mutationFn: async () => {
+      const response = await client.api.categories[":id"]["$delete"]({
+        param: { id: id }
+      })
+      return await response.json()
+    },
+    onSuccess: () => {
+      toast.success("Category deleted")
+      queryClient.invalidateQueries({ queryKey: ["category", { id }] })
+      queryClient.invalidateQueries({ queryKey: ["categories"] })
+      queryClient.invalidateQueries({ queryKey: ["transactions"] })
+      // TODO: Invalidate summary
+    },
+    onError: () => {
+      toast.error("Failed to delete category")
+    }
+  })
+
+  return mutation
+}
---
+++ features/categories/components/edit-category-sheet.tsx
@@ -0,0 +1,95 @@
+import { z } from "zod";
+import { Loader2 } from "lucide-react";
+
+import { useOpenCategory } from "@/features/categories/hooks/use-open-category";
+import { CategoryForm } from "@/features/categories/components/category-form";
+import { useEditCategory } from "@/features/categories/api/use-edit-category";
+import { useGetCategory } from "@/features/categories/api/use-get-category";
+import { useDeleteCategory } from "@/features/categories/api/use-delete-category";
+
+import { useConfirm } from "@/hooks/use-confirm";
+
+import { insertCategorySchema } from "@/db/schema";
+
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+} from "@/components/ui/sheet";
+
+const formSchema = insertCategorySchema.pick({
+  name: true,
+});
+
+type FormValues = z.input<typeof formSchema>;
+
+export const EditCategorySheet = () => {
+  const { isOpen, onClose, id } = useOpenCategory();
+  const [ConfirmDialog, confirm] = useConfirm(
+    "Are you sure?",
+    "You are about to delete this category."
+  );
+  const editMutation = useEditCategory(id);
+  const deleteMutation = useDeleteCategory(id);
+
+  const categoryQuery = useGetCategory(id);
+
+  const onSubmit = (values: FormValues) => {
+    editMutation.mutate(values, {
+      onSuccess: () => {
+        onClose();
+      },
+    });
+  };
+
+  const onDelete = async () => {
+    const ok = await confirm();
+    if (ok) {
+      deleteMutation.mutate(undefined, {
+        onSuccess: () => {
+          onClose();
+        },
+      });
+    }
+  };
+
+  const isPending = editMutation.isPending || deleteMutation.isPending;
+  const isLoading = categoryQuery.isLoading;
+
+  const defaultValues = categoryQuery?.data
+    ? {
+        name: categoryQuery.data.name,
+      }
+    : {
+        name: "",
+      };
+
+  return (
+    <>
+      <ConfirmDialog />
+      <Sheet open={isOpen} onOpenChange={onClose}>
+        <SheetContent className="space-y-4">
+          <SheetHeader>
+            <SheetTitle>Edit Category</SheetTitle>
+            <SheetDescription>Edit an existing category.</SheetDescription>
+          </SheetHeader>
+          {isLoading ? (
+            <div className="absolute inset-0 flex items-center justify-center">
+              <Loader2 className="size-4 text-muted-foreground animate-spin" />
+            </div>
+          ) : (
+            <CategoryForm
+              id={id}
+              onSubmit={onSubmit}
+              disabled={isPending}
+              defaultValues={defaultValues}
+              onDelete={onDelete}
+            />
+          )}
+        </SheetContent>
+      </Sheet>
+    </>
+  );
+};
