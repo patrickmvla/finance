54192410f226bb117fee01c14617e0159f0abdcd
Patrick Mvula <mvlapatrick@gmail.com>
2025-03-21 18:14:21 +0200

    feat: drizzle and neon db config

---
+++ drizzle/0000_oval_next_avengers.sql
@@ -0,0 +1,6 @@
+CREATE TABLE "accounts" (
+	"id" text PRIMARY KEY NOT NULL,
+	"name" text NOT NULL,
+	"user_id" text NOT NULL,
+	"plaid_id" text NOT NULL
+);
---
+++ src/db/schema.ts
@@ -0,0 +1,11 @@
+import { pgTable, text } from "drizzle-orm/pg-core";
+import { createInsertSchema } from 'drizzle-zod';
+
+export const accounts = pgTable("accounts", {
+  id: text("id").primaryKey(),
+  name: text("name").notNull(),
+  userId: text("user_id").notNull(),
+  plaidId: text("plaid_id").notNull(),
+});
+
+export const insertAccountSchema = createInsertSchema(accounts)
