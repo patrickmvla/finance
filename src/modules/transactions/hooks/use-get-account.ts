4468cfdf9c86b01d982c1fc692993f967ee93303
Patrick Mvula <mvlapatrick@gmail.com>
2025-03-28 11:32:48 +0200

    feat: transaction hooks

---
+++ src/modules/transactions/hooks/use-get-account.ts
@@ -0,0 +1,13 @@
+import { create } from "zustand";
+
+type GetAccountState = {
+  isOpen: boolean;
+  onOpen: () => void;
+  onClose: () => void;
+};
+
+export const useGetAccount = create<GetAccountState>((set) => ({
+  isOpen: false,
+  onOpen: () => set({ isOpen: true }),
+  onClose: () => set({ isOpen: false }),
+}));
