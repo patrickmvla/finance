9b9a5059afa04d27eddc82fcb6ec7ea15cc7e4d8
Patrick Mvula <mvlapatrick@gmail.com>
2025-04-03 16:25:23 +0200

    feat: category dash

---
+++ src/app/(dashboard)/categories/actions.tsx
@@ -0,0 +1,62 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { useDeleteCategory } from "@/modules/categories/api/use-delete-category";
+import { useOpenCategory } from "@/modules/categories/hooks/use-open-category";
+import { useConfirm } from "@/hooks/use-confirm";
+import { Edit, MoreHorizontal, Trash } from "lucide-react";
+
+type Props = {
+  id: string;
+};
+
+export const Actions = ({ id }: Props) => {
+  const { onOpen } = useOpenCategory();
+  const deleteMutation = useDeleteCategory(id);
+  const [ConfirmDialog, confirm] = useConfirm(
+    "Are you sure?",
+    "Your are about to delete this category"
+  );
+
+  const handleDelete = async () => {
+    const ok = await confirm();
+    if (ok) {
+      deleteMutation.mutate();
+    }
+  };
+
+  return (
+    <>
+      <ConfirmDialog />
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button className="size-8 p-0" variant={"ghost"}>
+            <MoreHorizontal className="size-4" />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end">
+          <DropdownMenuItem
+            disabled={deleteMutation.isPending}
+            onClick={() => onOpen(id)}
+          >
+            <Edit className="size-4 mr-2" />
+            Edit
+          </DropdownMenuItem>
+          <DropdownMenuItem
+            disabled={deleteMutation.isPending}
+            onClick={handleDelete}
+          >
+            <Trash className="size-4 mr-2" />
+            Delete
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </>
+  );
+};
---
+++ src/app/(dashboard)/categories/page.tsx
@@ -0,0 +1,63 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Loader2, PlusIcon } from "lucide-react";
+import { columns } from "./columns";
+import { DataTable } from "@/components/data-table";
+import { Skeleton } from "@/components/ui/skeleton";
+import { useNewCategory } from "@/modules/categories/hooks/use-new-category";
+import { useBulkDeleteCategories } from "@/modules/categories/api/use-bulk-delete";
+import { useGetCategories } from "@/modules/categories/api/use-get-categories";
+
+export default function CategoriesPage() {
+  const newCategory = useNewCategory();
+  const deleteAccounts = useBulkDeleteCategories();
+  const accountsQuery = useGetCategories();
+  const accounts = accountsQuery.data || [];
+
+  const isDisabled = accountsQuery.isLoading || deleteAccounts.isPending;
+
+  if (accountsQuery.isLoading) {
+    return (
+      <div className="max-w-screen-2xl mx-auto -mt-24 w-full pb-10">
+        <Card className="border-none drop-shadow-sm">
+          <CardHeader>
+            <Skeleton className="h-8 w-48" />
+          </CardHeader>
+          <CardContent>
+            <div className="h-[500px] w-full flex items-center justify-center">
+              <Loader2 className="size-6 text-slate-300 animate-spin" />
+            </div>
+          </CardContent>
+        </Card>
+      </div>
+    );
+  }
+
+  return (
+    <div className="max-w-screen-2xl mx-auto -mt-24 w-full pb-10">
+      <Card className="border-none drop-shadow-sm">
+        <CardHeader className="gap-y-2 lg:flex-row lg:items-center lg:justify-between">
+          <CardTitle className="text-xl line-clamp-1">Categories</CardTitle>
+          <Button size={"sm"} onClick={newCategory.onOpen}>
+            <PlusIcon className="size-4 mr-2" />
+            Add new
+          </Button>
+        </CardHeader>
+        <CardContent>
+          <DataTable
+            columns={columns}
+            data={accounts}
+            filterKey="name"
+            onDelete={(row) => {
+              const ids = row.map((r) => r.original.id);
+              deleteAccounts.mutate({ ids });
+            }}
+            disabled={isDisabled}
+          />
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
